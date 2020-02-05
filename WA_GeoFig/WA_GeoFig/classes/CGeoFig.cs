418dd69fc8e295fb8b2c925365b5528ae861cf9a
Sven Greinert <svengreinert@outlook.com>
2020-02-05 13:26:35 +0100

    Added all Classes

---
+++ WA_GeoFig/WA_GeoFig/classes/CGeoFig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WA_GeoFig
+{
+    class CGeoFig
+    {
+        string name;
+        string color;
+
+        public string Name { get => name; set => name = value; }
+        public string Color { get => color; set => color = value; }
+    }
+}
---
+++ WA_GeoFig/WA_GeoFig/classes/body/CBody.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WA_GeoFig
+{
+    class CBody : CGeoFig
+    {
+        float volume;
+        float surfaceArea;
+
+        public float Volume { get => volume; set => volume = value; }
+        public float SurfaceArea { get => surfaceArea; set => surfaceArea = value; }
+    }
+}
